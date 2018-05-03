8f7bfc3c80879a55e56affbc817ccf44c3811b3f
sansansisi <18601343344@163.com>
2018-05-03 10:23:42 +0800

    fix README.md

---
+++ SSIDCard/Classes/Public/Category/UIView+SS.h
@@ -0,0 +1,12 @@
+//
+//  UIView+SS.h
+//  SSIDCard
+//
+//  Created by 张家铭 on 2018/5/3.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (SS)
+
+@end
---
+++ SSIDCard/Classes/Public/Category/UIView+SS.m
@@ -0,0 +1,12 @@
+//
+//  UIView+SS.m
+//  SSIDCard
+//
+//  Created by 张家铭 on 2018/5/3.
+//
+
+#import "UIView+SS.h"
+
+@implementation UIView (SS)
+
+@end
